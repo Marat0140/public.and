49b79f4234c4f04621f0f55f577617433310ad32
marat0140 <maratcamargo06@gmail.com>
2024-08-03 15:28:18 -0400

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,7 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
-
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak -$30', 'Pasta -$6', 'Burger -$10', 'Salmon -$7'];
+const dessertMenu = ['Cake -$9', 'Ice Cream -$3', 'Pudding -$6', 'Fruit Salad -$5'];
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
